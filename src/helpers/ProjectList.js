aa5702b84bf0445e6ff585b1f76db71d751d6d67
dobyming <12184874@inha.edu>
2023-04-11 10:10:31 +0900

    Frame project components

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,28 @@
+import Project1 from '../assets/project1_pic.jpg';
+import Project2 from '../assets/project2_pic.jpg';
+import Project3 from '../assets/project3_pic.jpg';
+import Project4 from '../assets/project4_pic.jpg';
+import Project5 from '../assets/project5_pic.jpg';
+
+export const ProjectList = [
+    {
+        name : "Emotionary",
+        image: Project1
+    },
+    {
+        name : "Covid19 Board",
+        image: Project2
+    },
+    {
+        name : "Cat meme pics Creator",
+        image: Project3
+    },
+    {
+        name : "Restaurant Map",
+        image: Project4
+    },
+    {
+        name : "Img Auto Labeling Tool",
+        image: Project5
+    },
+]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem';
+import '../styles/Projects.css';
+import { ProjectList } from '../helpers/ProjectList';
 
-function Projects() {
+const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projectList'>
+        {ProjectList.map((it) => {
+          return <ProjectItem name = {it.name} image ={it.image}/>
+        })}
+      </div>
+    </div>
   )
 }
 
